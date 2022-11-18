1437a05549e06a72cdc0d72d4fa9386210c3852b
NatTonetti <tonettinatacha@gmail.com>
2022-11-17 23:16:47 -0300

    Commit inicial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React ,{useState,useEffect}from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+export default function App() {
+  const [Api,setApi]=useState()
+
+  useEffect(()=>{
+    fetch(`https://kitsu.io/api/edge/anime?filter[text]=naruto`)
+    .then(res => res.json())
+    .then(res=> {
+      setApi(res)
+    })
+
+  },[])
+
+
+  return(
+    <div class='main-container'>
+      {Api !== undefined ?
+      <div class='sub-container' >
+        {Api.data.slice(0, 8).map((info,id)=>(
+          <div class='map-container' style={{backgroundColor:`#${id}${id}${id}`}} key={id}>
+            <h4 class='title'>{info.attributes.slug}</h4>
+            <div class='img-container'>
+            <img class='img-poster' src={info.attributes.posterImage.small}/>
+            </div>
+            </div>
+        ))}
+      </div>
+
+
+     :null }
+
+
+
     </div>
-  );
+
+  )
 }
 
-export default App;
